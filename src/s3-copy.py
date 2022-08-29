9567fe4842bc03fee441ea77ddfcef23437e22ed
Sergey Frolov <frolovsa@gmail.com>
2022-08-29 22:10:13 +0000

    added functionality to skip copy if file already present

--- src/s3-copy.py
+++ src/s3-copy.py
@@ -33,11 +33,15 @@ destination = config_dict.get('destination')
 print('Opening aws resources')
 destination_bucket = boto3.Session(profile_name=destination.get('credential_profile')).resource('s3').Bucket(destination["bucket"])
 destination_fn_template = destination.get('path')+destination["file_template"]["prefix"]+destination["file_template"]["root"]+destination["file_template"]["suffix"]
+source_buckets = []
+for source in sources.values():
+  source_buckets.append( boto3.Session(profile_name=source["credential_profile"]).resource('s3').Bucket(source["bucket"]))
 print('...Done')
 
 #----- open log files
 print('Opening loggers')
 dtg = datetime.now().strftime('%Y%m%d%H%M%S')
+logger_present = open(config_dict["logging"]["present"].format(dtg=dtg),'wt')
 logger_success = open(config_dict["logging"]["success"].format(dtg=dtg),'wt')
 logger_missing = []
 si = 0
@@ -62,13 +66,29 @@ while not(dr.at_end()):
     # construct source filename
     fn_template = source["file_template"]["prefix"]+source["file_template"]["root"]+source["file_template"]["suffix"]
     source_dict = {"Bucket" : source["bucket"], "Key" : current_cycle.strftime(source["key"]+fn_template)}
-    # if source file exists, then copy
+
+    # check first if destination exists and it is the same as source
+    try: md5destination = destination_bucket.Object(destination_key).e_tag[1 :-1]
+    except: pass
+    else: #destination file exists
+      try: md5source = source_buckets[si].Object(source_dict["Key"]).e_tag[1 :-1]
+      except: pass
+      else: #source file exists
+        if (md5source == md5destination):
+          # log that this file is missing
+          logger_present.write("s3://{}/{} == s3://{}/{}\n".format(source_dict["Bucket"], source_dict["Key"], destination["bucket"], destination_key))
+          print(f" present {si}")
+          # skip to next, no need to copy identical file
+          break
+
+    # if source file exists but the destiantion doesnt, then copy
     try:
       destination_bucket.copy( source_dict, destination_key)
     except botocore.exceptions.ClientError as e:
       if e.response['Error']['Code'] == "404":
         # log that the file is missing and try next source
         logger_missing[si].write("s3://{}/{}\n".format(source_dict["Bucket"], source_dict["Key"]));
+        print(f" missing {si}")
         continue
       else:
         # something else went wrong
@@ -76,12 +96,16 @@ while not(dr.at_end()):
     else:
       # log success message
       logger_success.write("s3://{}/{} -> s3://{}/{}\n".format(source_dict["Bucket"], source_dict["Key"], destination["bucket"], destination_key))
+      print(f" copy from {si}")
+      break
+  # increment time counter
   dr.increment(seconds = cycling_interval)
 print('done with the time loop')
 
 #----- close logging files
 print('closing loggers')
 logger_success.close()
+logger_present.close()
 for l in logger_missing:
   l.close()
 print('at the end')
