f82cdc7a03d1dae77fa185565625a65b47910734
Sergey Frolov <frolovsa@gmail.com>
2022-08-30 09:25:17 -0600

    correcting errors in amsua and prepbufr uploads

---
+++ data-tree/reanalysis/conv/gefs-prepbufr/remove_wrong_2000-2010.sh
@@ -0,0 +1,15 @@
+aws s3 rm --profile=rnr-bdp --recursive s3://noaa-reanalyses-pds/observations/reanalysis/conv/gefs-prepbufr/2000/
+aws s3 rm --profile=rnr-bdp --recursive s3://noaa-reanalyses-pds/observations/reanalysis/conv/gefs-prepbufr/2001/
+aws s3 rm --profile=rnr-bdp --recursive s3://noaa-reanalyses-pds/observations/reanalysis/conv/gefs-prepbufr/2002/
+aws s3 rm --profile=rnr-bdp --recursive s3://noaa-reanalyses-pds/observations/reanalysis/conv/gefs-prepbufr/2003/
+aws s3 rm --profile=rnr-bdp --recursive s3://noaa-reanalyses-pds/observations/reanalysis/conv/gefs-prepbufr/2004/
+aws s3 rm --profile=rnr-bdp --recursive s3://noaa-reanalyses-pds/observations/reanalysis/conv/gefs-prepbufr/2005/
+aws s3 rm --profile=rnr-bdp --recursive s3://noaa-reanalyses-pds/observations/reanalysis/conv/gefs-prepbufr/2006/
+aws s3 rm --profile=rnr-bdp --recursive s3://noaa-reanalyses-pds/observations/reanalysis/conv/gefs-prepbufr/2007/
+aws s3 rm --profile=rnr-bdp --recursive s3://noaa-reanalyses-pds/observations/reanalysis/conv/gefs-prepbufr/2008/
+aws s3 rm --profile=rnr-bdp --recursive s3://noaa-reanalyses-pds/observations/reanalysis/conv/gefs-prepbufr/2009/
+aws s3 rm --profile=rnr-bdp --recursive s3://noaa-reanalyses-pds/observations/reanalysis/conv/gefs-prepbufr/2010/01/
+aws s3 rm --profile=rnr-bdp --recursive s3://noaa-reanalyses-pds/observations/reanalysis/conv/gefs-prepbufr/2010/02/
+aws s3 rm --profile=rnr-bdp --recursive s3://noaa-reanalyses-pds/observations/reanalysis/conv/gefs-prepbufr/2010/03/
+aws s3 rm --profile=rnr-bdp --recursive s3://noaa-reanalyses-pds/observations/reanalysis/conv/gefs-prepbufr/2010/04/
+
